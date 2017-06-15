1b8fbae28ed7182ce9df0df4587508b85b035e3a
Mateusz Siek <mateuszsiek91@gmail.com>
2017-06-15 19:06:24 +0100

    result card init module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import {SearchModule} from "./search/search.module";
+import { SearchModule } from "./search/search.module";
+import { ResultCardModule } from "./result-card/result-card.module";
+import { RepositoryDetailsComponent } from './repository-card/repository-details/repository-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryDetailsComponent
   ],
   imports: [
     BrowserModule,
-    SearchModule
+    SearchModule,
+    ResultCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
